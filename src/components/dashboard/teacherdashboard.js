4e5e32a4f221867dc9db72c2427bd84003cf8895
Gnyani <gnyani007@gmail.com>
2018-07-11 23:10:17 +0530

    adding ui for adding class for teacher

--- src/components/dashboard/teacherdashboard.js
+++ src/components/dashboard/teacherdashboard.js
@@ -2,6 +2,10 @@ import React,{Component} from 'react';
 import {Link} from 'react-router-dom';
 import {red500} from 'material-ui/styles/colors';
 import MenuItem from 'material-ui/MenuItem';
+import FlatButton from 'material-ui/FlatButton';
+import Add from 'material-ui/svg-icons/content/add'
+import Dialog from 'material-ui/Dialog'
+import DisplayBatches from './DisplayBatches'
 import {LocationCity} from '../../styledcomponents/SvgIcons.js'
 
 
@@ -11,6 +15,9 @@ class TeacherDashboard extends Component{
     super();
     this.state={
       selected: '',
+      addClassDialog: false,
+      year: 0,
+      section: 0,
     }
     this.isActive = this.isActive.bind(this)
   }
@@ -19,6 +26,25 @@ class TeacherDashboard extends Component{
       selected: value
     })
   }
+
+ handleSectionChange = (e, index, value) => {
+   this.setState({
+     section: value,
+   })
+ }
+
+  handleYearChange = (e, index, value) => {
+    this.setState({
+      year : value,
+    })
+  }
+
+  openDialog = () => {
+    this.setState({
+      addClassDialog: true,
+    })
+  }
+
   isActive(value){
       return (value === this.state.selected)?'Active':'';
   }
@@ -38,9 +64,44 @@ class TeacherDashboard extends Component{
       </div>
     ));
   }
+
+  handleClose = () => {
+    this.setState({
+     addClassDialog: false,
+    })
+  }
+
   render(){
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={this.handleClose}
+      />,
+      <FlatButton
+        label="Confirm"
+        primary={true}
+        onTouchTap={this.addClass}
+      />]
     return(
-      <div>{this.menuItems(this.props.batches)}</div>
+      <div>
+        {this.menuItems(this.props.batches)}
+        <FlatButton type="button" label="Add Batch" fullWidth={true}  icon={<Add color={"white"}/>}
+          className="drawerFont" onClick={this.openDialog}/>
+          <Dialog
+                title="Select batch to add"
+                modal={false}
+                actions={actions}
+                open={this.state.addClassDialog}
+                titleStyle={{textAlign:"center",color: "rgb(162,35,142)"}}
+                onRequestClose={this.handleClose}
+              >
+            <DisplayBatches key={this.state.year} year={this.state.year}
+              section={this.state.section}
+              handleSectionChange={this.handleSectionChange}
+              handleYearChange={this.handleYearChange} />
+          </Dialog>
+      </div>
     )
   }
 }
