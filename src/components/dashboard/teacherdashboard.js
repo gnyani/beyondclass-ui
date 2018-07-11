9d42fab8d9f43380c34c197bdff6dd30bc5ebc1d
Samantula <Pratapraj.Samantula@in.pega.com>
2018-07-12 00:42:32 +0530

    listing counts in teacher UI

--- src/components/dashboard/teacherdashboard.js
+++ src/components/dashboard/teacherdashboard.js
@@ -3,15 +3,19 @@ import {Link} from 'react-router-dom';
 import {red500} from 'material-ui/styles/colors';
 import MenuItem from 'material-ui/MenuItem';
 import {LocationCity} from '../../styledcomponents/SvgIcons.js'
+import Badge from 'material-ui/Badge';
+import People from 'material-ui/svg-icons/social/people.js'
 
 
 class TeacherDashboard extends Component{
 
-  constructor(){
-    super();
+  constructor(props){
+
+    super(props);
     this.state={
       selected: '',
     }
+    console.log(props.studentCountList)
     this.isActive = this.isActive.bind(this)
   }
   onChangeSelected(value){
@@ -28,8 +32,21 @@ class TeacherDashboard extends Component{
     <div key={index}>  <div  className={this.isActive(this.props.batches[index])}>
          <Link to={'/teacher/'+this.props.batches[index]}  width={this.props.width} style={{ textDecoration: 'none' }} onClick={this.onChangeSelected.bind(this,this.props.batches[index])} >
           <MenuItem
-          primaryText={'Class  '+this.props.batches[index]}
-          leftIcon={<LocationCity color={red500}/>}
+          primaryText={'\n'+ 'Class  '+this.props.batches[index]}
+          innerDivStyle={{
+            paddingTop: "5px",
+          }}
+          rightIcon={
+            <Badge
+             badgeContent={this.props.studentCountList[batches]}
+             primary={true}
+             badgeStyle={{ width: "24px", height: "24px"}}
+             style={{
+               padding: "0px 24px 12px 12px"
+             }}
+           >
+             <People color={"white"} viewBox={0,0,50,50}/>
+           </Badge>}
           onClick={this.props.handleMobileToggle}
           className="drawerFont"
            />
