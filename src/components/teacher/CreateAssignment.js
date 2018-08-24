25db0e0e5326bc29c604d71c2cedd93dfc0cccb7
vishnumahi <vishnuhiphop.leen12@gmail.com>
2018-08-25 02:21:52 +0530

    floating add assignments button

--- src/components/teacher/CreateAssignment.js
+++ src/components/teacher/CreateAssignment.js
@@ -7,12 +7,16 @@ import ListAssignments from './ListAssignments'
 import {Link} from 'react-router-dom'
 import { withRouter } from 'react-router'
 import PropTypes from 'prop-types'
+import IconMenu from 'material-ui/IconMenu'
+import MenuItem from 'material-ui/MenuItem'
+import IconButton from 'material-ui/IconButton'
+import { white, transparent } from 'material-ui/styles/colors'
 
 
 class CreateAssignment extends Component{
   render(){
     return(<div>
-      <Grid fluid>
+      {/* <Grid fluid>
       <Row center='xs'>
       <Col xs>
       <br />
@@ -33,9 +37,41 @@ class CreateAssignment extends Component{
       <br /> <br />
       </Col>
       </Row>
-      </Grid>
+      </Grid> */}
       <Divider />
       <br />
+      <Grid flex>
+      <Row around='xs'>
+        <Col xs={12} sm={12} md={12} lg={12} className="fab-btn">
+        <IconMenu iconButtonElement={
+                <IconButton
+                  touch={true}
+                  tooltip="Create Assignments"
+                  tooltipPosition="top-left"
+                  className="fab-icon-btn"
+                >
+                  <Add className="whitecol"/>
+                </IconButton>
+               }>
+          <Link style={{textDecoration: 'none'}} to={'/teacher/'+this.props.class+'/create'} >
+            <MenuItem >
+            Theory Assignment
+            </MenuItem>
+          </Link>
+          <Link style={{textDecoration: 'none'}} to={'/teacher/'+this.props.class+'/createobjectiveassignment'} >
+            <MenuItem >
+            Objective Assignment
+            </MenuItem>
+          </Link>
+          <Link style={{textDecoration: 'none'}} to={'/teacher/'+this.props.class+'/createpa'} >
+            <MenuItem >
+            Programming Assignment
+            </MenuItem>
+          </Link>
+        </IconMenu>
+        </Col>
+      </Row>
+    </Grid>
       <ListAssignments batches={this.props.batches} class={this.props.class} email={this.props.email}  branch={this.props.branch}/>
       </div>)
   }
